1306a76c39fbcda017688ea01107e742f9b5db61
oxuegen <dubrovskay.7830@mail.ru>
2021-09-11 09:51:21 +0300

    Initial commit

---
+++ app/src/main/java/dfi/fit/bstu/by/MainActivity.java
@@ -0,0 +1,14 @@
+package dfi.fit.bstu.by;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//TODO ADD NEW METHOD
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
